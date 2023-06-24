8e26103cc5ea81af630026055a92ec105a8040ce
Sidharth Mohanty <sidmohanty11@gmail.com>
2023-06-25 00:18:48 +0530

    create community

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import '@/styles/globals.css'
 import { Inter } from 'next/font/google'
 import Navbar from '@/components/navbar/Navbar'
 import { Toaster } from '@/components/ui/toaster'
+import Providers from '@/components/Providers'
 
 export const metadata = {
   title: 'Thread_It',
@@ -21,13 +22,15 @@ export default function RootLayout({
   return (
     <html lang='en' className={cn('bg-white text-slate-900 antialiased light', inter.className)}>
       <body className='min-h-screen pt-12 bg-slate-50 antialiased'>
-        {/* @ts-expect-error server component */}
-        <Navbar />
-        {authModal}
-        <div className='container max-w-7xl mx-auto h-full pt-12'>
-          {children}
-        </div>
-        <Toaster />
+        <Providers>
+          {/* @ts-expect-error server component */}
+          <Navbar />
+          {authModal}
+          <div className='container max-w-7xl mx-auto h-full pt-12'>
+            {children}
+          </div>
+          <Toaster />
+        </Providers>
       </body>
     </html>
   )
